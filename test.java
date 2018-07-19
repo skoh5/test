22b225e8d763e8b19ffbea1144ebf8f902a8a06a
skoh5 <ohsg74@gmail.com>
2018-07-20 08:48:57 +0900

    Update test.java

--- test.java
+++ test.java
@@ -9,3 +9,11 @@ void sha256(String input) {
 
 // net - https://gist.github.com/junsuk5/f0ff2298e17853dc48e89f2dfc7bd985
 // lock - https://winterbe.com/posts/2015/04/30/java8-concurrency-tutorial-synchronized-locks-examples/
+// keyin
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+while (true) {
+  String strInput = br.readLine();
+}
+
+//list sort
+Collections.sort(al, (g1, g2) -> g1.getName().compareTo(g2.getName()));
