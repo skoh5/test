3cdb9d1351270dcd63acfe9a984f47f6560e0b2f
skoh5 <ohsg74@gmail.com>
2018-08-22 08:06:43 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,4 @@
+// make hash string
 void sha256(String input) {
   MessageDigest digest = MessageDigest.getInstance("SHA-256")
   byte[] result = digest.digest(input.getBytes());
@@ -8,18 +9,20 @@ void sha256(String input) {
 }
 
 // net - https://gist.github.com/junsuk5/f0ff2298e17853dc48e89f2dfc7bd985
+
 // lock - https://winterbe.com/posts/2015/04/30/java8-concurrency-tutorial-synchronized-locks-examples/
+
 // keyin
 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 while (true) {
   String strInput = br.readLine();
 }
 
-//list sort
+//list sort - 
 Collections.sort(al, (g1, g2) -> g1.getName().compareTo(g2.getName()));
 
-// chain
-//http://www.bitweb.co.kr/news/list.php?mcode=m659o6x
+// block chain
+// http://www.bitweb.co.kr/news/list.php?mcode=m659o6x
 // https://www.popit.kr/java-%EC%BD%94%EB%93%9C%EB%A1%9C-%EC%9D%B4%ED%95%B4%ED%95%98%EB%8A%94-%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8blockchain/
 
 //external exec
@@ -29,4 +32,13 @@ http://blog.daum.net/_blog/BlogTypeView.do?blogid=0Nz1O&articleno=69&categoryId=
 http://hochulshin.com/java-multithreading-executor-basic/
 https://winterbe.com/posts/2015/04/07/java8-concurrency-tutorial-thread-executor-examples/
 
+// 최대공약수
+private long gcd(int p, int q) {
+	if(q == 0) return p;
+	return gcd(q, p%q);
+}
 
+// 최소공배수	
+private long lcm(int p, int q) {
+	return (long)(p*q)/gcd(p, q);
+}
