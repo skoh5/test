8061c9e5ea39a1171c90fc19ee8efb769b454d16
skoh5 <ohsg74@gmail.com>
2018-09-12 13:19:26 +0900

    Update test.java

--- test.java
+++ test.java
@@ -47,3 +47,39 @@ private long lcm(int p, int q) {
 // from, where, rownum, group by, having, select, order by
 // TO_DATE(... , 'YYYY/MM/DD HH24:MI:SS')
 
+def snailNumber(array, r, c):
+
+    lst = list()
+
+    cnt = 0         # 현재 출력할 수
+    curr_point = (0,0)  # 현재 수를 출력할 좌표
+    curr_direct = 'RIGHT'   # 현재 방향
+
+    # 방향을 넣으면 움직여야할 좌표를 반환하는 dictionary
+    # 이차원 리스트에서 움직일 수있는 방향은 상하좌우 4개의 방향
+    move = {'UP': (-1,0),'DOWN': (1,0),'LEFT': (0,-1),'RIGHT': (0,1)}
+
+    # 현재 방향을 넣으면 다음방향을 반환하는 dictionary
+    next_direct = {'UP': 'RIGHT','RIGHT': 'DOWN','DOWN': 'LEFT','LEFT': 'UP'}
+
+    # 방향이 바뀔때 마다 움직여야할 거리를 순서대로 list에 담음
+    for i in range(r+c-1):  
+        if i % 2 is 0:
+            lst.append(c)
+            r = r - 1            
+        else:
+            lst.append(r)
+            c = c - 1
+    print(lst)
+ # array에 차례로 숫자를 씀
+#------------------------------------------------
+# 이 부분에 코딩하시오.
+
+    for tmp in lst :
+        i = 0
+        for i in range(0, tmp) :
+            array[curr_point[0]][curr_point[1]] = cnt
+            cnt = cnt + 1
+            if (i == tmp - 1):
+                curr_direct = next_direct[curr_direct]
+            curr_point = (curr_point[0] + move[curr_direct][0], curr_point[1] + move[curr_direct][1])
